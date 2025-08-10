5587355bf3c4dd321c705c5e183ed3ba7f73751c
Hector Josue Dominguez Hinojosa <hectorjosue78@gmail.com>
2025-08-10 09:09:49 -0600

    Se cambio el auth de frontend

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -50,5 +50,5 @@ class Auth {
 }
 
 export const auth = new Auth({
-  baseUrl: "https://se-register-api.en.tripleten-services.com/v1",
+  baseUrl: "http://api.hectorvmbootcamp.chickenkiller.com",
 });
